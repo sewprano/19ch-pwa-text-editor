3e6c3ec8655903de4ebef7207925d6da042b389d
Deborah Wade <sopranosamurai@yahoo.com>
2022-12-05 10:53:23 -0800

    sw plugins, loaders and babel in wepback.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new WebpackPwaManifest( {
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Take Notes with Javascript Syntax Highlighting',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        publicPath: '/',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve( 'src/images/logo.png' ),
+            sizes: [ 96, 128, 192, 256, 384, 512 ],
+            destination: path.join( 'assets', 'icons' )
+          }
+        ]
+      } ),
     ],
 
     module: {
       rules: [
         
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        },
       ],
     },
   };
